2f6443aab72bde01f039b07b6fe438814c8dd732
André Marcelino <andre.marcelino@fatec.sp.gov.br>
2026-04-21 11:41:56 -0300

    carregamento imagens

--- src/resources/views/site/home/features.blade.php
+++ src/resources/views/site/home/features.blade.php
@@ -1,69 +1,92 @@
 <!-- Features Section Two -->
 
 @php
-$features = asset('davilla/IMG/there-are-many-different-types-pastries-pink-plate.jpg')
+    $features = asset('davilla/IMG/there-are-many-different-types-pastries-pink-plate.jpg');
 @endphp
-    <div class="features-section-two">
-        <div class="shape_wrapper shape_four_up">
-            <div class="shape_inner shape_four_down" style="background-image: url('{{$features}}');"><div class="overlay"></div></div>
+<div class="features-section-two">
+    <div class="shape_wrapper shape_four_up">
+        <div class="shape_inner shape_four_down" style="background-image: url('{{ $features }}');">
+            <div class="overlay"></div>
         </div>
+    </div>
 
-        <div class="auto-container">
-            <div class="row">
-                <!-- Feature Block Two -->
-                <div class="feature-block-two col-lg-4 col-md-6 col-sm-12">
-                    <div class="inner-box">
-                        <div class="icon-box">
-                            <div class="icon-frame">
-                                <svg viewBox="0 0 850.4 850.4"><path d="M436.7,850.4C195.9,850.4,0,659.7,0,425.2c0-10.4,8.2-18.8,18.4-18.8s18.4,8.4,18.4,18.8c0,213.7,179.4,387.6,399.9,387.6 c214.8,0,376.8-168.7,376.8-392.3c0-217.9-161.4-382.3-375.6-382.9C244.6,38.2,87.5,198.8,87.5,396.2 C87.5,594,245.2,754.9,439,754.9c10.2,0,18.4,8.4,18.4,18.8c0,10.4-8.2,18.8-18.4,18.8c-214.1,0-388.4-177.7-388.4-396.2 C50.7,178.5,223.6,1.3,436.7,0v0l2.3,0c234.7,1.2,411.4,181.5,411.4,420.5c0,115.2-41.2,222.7-116.1,302.7 C657.5,805.2,551.8,850.4,436.7,850.4z"></path></svg>
-                            </div>
-                            <!-- cake img -->
-                            <div class="icon flaticon-food-6"></div>
+    <div class="auto-container">
+        <div class="row">
+            <!-- Feature Block Two -->
+            <div class="feature-block-two col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box">
+                    <div class="icon-box">
+                        <div class="icon-frame">
+                            <svg viewBox="0 0 850.4 850.4">
+                                <path
+                                    d="M436.7,850.4C195.9,850.4,0,659.7,0,425.2c0-10.4,8.2-18.8,18.4-18.8s18.4,8.4,18.4,18.8c0,213.7,179.4,387.6,399.9,387.6 c214.8,0,376.8-168.7,376.8-392.3c0-217.9-161.4-382.3-375.6-382.9C244.6,38.2,87.5,198.8,87.5,396.2 C87.5,594,245.2,754.9,439,754.9c10.2,0,18.4,8.4,18.4,18.8c0,10.4-8.2,18.8-18.4,18.8c-214.1,0-388.4-177.7-388.4-396.2 C50.7,178.5,223.6,1.3,436.7,0v0l2.3,0c234.7,1.2,411.4,181.5,411.4,420.5c0,115.2-41.2,222.7-116.1,302.7 C657.5,805.2,551.8,850.4,436.7,850.4z">
+                                </path>
+                            </svg>
                         </div>
-                        <h3>Tradition</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat risus quam, eget varius metus gravida vel.</p>
-                        <div class="btn-box">
-                            <a href="#" class="theme-btn btn-style-one regular"><span></span>Know More<span></span></a>
-                        </div>
+                        <!-- cake img -->
+                        <div class="icon flaticon-food-6"></div>
                     </div>
-                </div>                
-
-                <!-- Feature Block Two -->
-                <div class="feature-block-two col-lg-4 col-md-6 col-sm-12">
-                    <div class="inner-box">
-                        <div class="icon-box">
-                            <div class="icon-frame">
-                                <svg viewBox="0 0 850.4 850.4"><path d="M436.7,850.4C195.9,850.4,0,659.7,0,425.2c0-10.4,8.2-18.8,18.4-18.8s18.4,8.4,18.4,18.8c0,213.7,179.4,387.6,399.9,387.6 c214.8,0,376.8-168.7,376.8-392.3c0-217.9-161.4-382.3-375.6-382.9C244.6,38.2,87.5,198.8,87.5,396.2 C87.5,594,245.2,754.9,439,754.9c10.2,0,18.4,8.4,18.4,18.8c0,10.4-8.2,18.8-18.4,18.8c-214.1,0-388.4-177.7-388.4-396.2 C50.7,178.5,223.6,1.3,436.7,0v0l2.3,0c234.7,1.2,411.4,181.5,411.4,420.5c0,115.2-41.2,222.7-116.1,302.7 C657.5,805.2,551.8,850.4,436.7,850.4z"></path></svg>
-                            </div>
-                            <!-- cake img -->
-                            <div class="icon flaticon-food-13"></div>
-                        </div>
-                        <h3>Creatiwity</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat risus quam, eget varius metus gravida vel.</p>
-                        <div class="btn-box">
-                            <a href="#" class="theme-btn btn-style-one regular"><span></span>Know More<span></span></a>
-                        </div>
+                    <h3>Tradition</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat risus quam, eget varius
+                        metus gravida vel.</p>
+                    <div class="btn-box">
+                        <a href="#" class="theme-btn btn-style-one" style="font-size: 14px; padding: 10px 20px;">
+                            <span></span>Veja nossa vitrine<span></span>
+                        </a>
                     </div>
                 </div>
+            </div>
 
-                <!-- Feature Block Two -->
-                <div class="feature-block-two col-lg-4 col-md-6 col-sm-12">
-                    <div class="inner-box">
-                        <div class="icon-box">
-                            <div class="icon-frame">
-                                <svg viewBox="0 0 850.4 850.4"><path d="M436.7,850.4C195.9,850.4,0,659.7,0,425.2c0-10.4,8.2-18.8,18.4-18.8s18.4,8.4,18.4,18.8c0,213.7,179.4,387.6,399.9,387.6 c214.8,0,376.8-168.7,376.8-392.3c0-217.9-161.4-382.3-375.6-382.9C244.6,38.2,87.5,198.8,87.5,396.2 C87.5,594,245.2,754.9,439,754.9c10.2,0,18.4,8.4,18.4,18.8c0,10.4-8.2,18.8-18.4,18.8c-214.1,0-388.4-177.7-388.4-396.2 C50.7,178.5,223.6,1.3,436.7,0v0l2.3,0c234.7,1.2,411.4,181.5,411.4,420.5c0,115.2-41.2,222.7-116.1,302.7 C657.5,805.2,551.8,850.4,436.7,850.4z"></path></svg>
-                            </div>
-                            <!-- cake img -->
-                            <div class="icon flaticon-food-14"></div>
+            <!-- Feature Block Two -->
+            <div class="feature-block-two col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box">
+                    <div class="icon-box">
+                        <div class="icon-frame">
+                            <svg viewBox="0 0 850.4 850.4">
+                                <path
+                                    d="M436.7,850.4C195.9,850.4,0,659.7,0,425.2c0-10.4,8.2-18.8,18.4-18.8s18.4,8.4,18.4,18.8c0,213.7,179.4,387.6,399.9,387.6 c214.8,0,376.8-168.7,376.8-392.3c0-217.9-161.4-382.3-375.6-382.9C244.6,38.2,87.5,198.8,87.5,396.2 C87.5,594,245.2,754.9,439,754.9c10.2,0,18.4,8.4,18.4,18.8c0,10.4-8.2,18.8-18.4,18.8c-214.1,0-388.4-177.7-388.4-396.2 C50.7,178.5,223.6,1.3,436.7,0v0l2.3,0c234.7,1.2,411.4,181.5,411.4,420.5c0,115.2-41.2,222.7-116.1,302.7 C657.5,805.2,551.8,850.4,436.7,850.4z">
+                                </path>
+                            </svg>
                         </div>
-                        <h3>Passion</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat risus quam, eget varius metus gravida vel.</p>
-                        <div class="btn-box">
-                            <a href="#" class="theme-btn btn-style-one regular"><span></span>Know More<span></span></a>
+                        <!-- cake img -->
+                        <div class="icon flaticon-food-13"></div>
+                    </div>
+                    <h3>Creatiwity</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat risus quam, eget varius
+                        metus gravida vel.</p>
+                    <div class="btn-box">
+                        <a href="#" class="theme-btn btn-style-one" style="font-size: 14px; padding: 10px 20px;">
+                            <span></span>Veja nossa vitrine<span></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Feature Block Two -->
+            <div class="feature-block-two col-lg-4 col-md-6 col-sm-12">
+                <div class="inner-box">
+                    <div class="icon-box">
+                        <div class="icon-frame">
+                            <svg viewBox="0 0 850.4 850.4">
+                                <path
+                                    d="M436.7,850.4C195.9,850.4,0,659.7,0,425.2c0-10.4,8.2-18.8,18.4-18.8s18.4,8.4,18.4,18.8c0,213.7,179.4,387.6,399.9,387.6 c214.8,0,376.8-168.7,376.8-392.3c0-217.9-161.4-382.3-375.6-382.9C244.6,38.2,87.5,198.8,87.5,396.2 C87.5,594,245.2,754.9,439,754.9c10.2,0,18.4,8.4,18.4,18.8c0,10.4-8.2,18.8-18.4,18.8c-214.1,0-388.4-177.7-388.4-396.2 C50.7,178.5,223.6,1.3,436.7,0v0l2.3,0c234.7,1.2,411.4,181.5,411.4,420.5c0,115.2-41.2,222.7-116.1,302.7 C657.5,805.2,551.8,850.4,436.7,850.4z">
+                                </path>
+                            </svg>
                         </div>
+                        <!-- cake img -->
+                        <div class="icon flaticon-food-14"></div>
+                    </div>
+                    <h3>Passion</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc feugiat risus quam, eget varius
+                        metus gravida vel.</p>
+                    <div class="btn-box">
+                     <a href="#" class="theme-btn btn-style-one" style="font-size: 14px; padding: 10px 20px;">
+                            <span></span>Veja nossa vitrine<span></span>
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!-- End Features Section -->
+</div>
+<!-- End Features Section -->
--- src/resources/views/site/home/fluid.blade.php
+++ src/resources/views/site/home/fluid.blade.php
@@ -1,31 +1,42 @@
-@php
-$fluid = asset('davilla/IMG/pink-cupcakes-donuts-with-hearts-pink-background.jpg');
-@endphp
+  <!-- Fluid Section one -->
 
-<section class="fluid-section-one" style="background-image: url('{{$fluid}}');">
-    <div class="outer-box clearfix">
-        <div class="content-column left-column">
-     
-        </div>
+  @php
+      $fluid = asset('davilla/IMG/assorted-delicious-cookies-pastries-with-berries-dark-background.jpg');
+  @endphp
+  <section class="fluid-section-one" style="background-image: url('{{ $fluid }}');">
+      <div class="outer-box clearfix">
+          <!-- Content Column -->
+          <div class="content-column left-column">
+              <div class="inner-column">
+                  <h2>A Melhor <br>Confeitaria da Região zona Leste</h2>
 
-        <div class="image-column">
-            <div class="image-box">
-                <figure class="image">
-                    {{-- Caminho corrigido para a pasta public/davilla/images --}}
-                    <img src="{{asset('davilla/images/icons/divider_2_light.png')}}" alt="">
-                </figure>
-            </div>
-        </div>
+     <p style="
+    text-align: justify; 
+    hyphens: auto; 
+    font-size: 1.5rem; 
+    line-height: 1.4; 
+    color: #222; 
+    text-shadow: 1px 1px 2px rgba(255,255,255,0.8);
+">
+    Na <span style="color: #d81b60; font-weight: bold; border-bottom: 2px solid #ffb6c1;">Confeitaria Davilla</span>, 
+    o aroma do café fresquinho encontra a delicadeza da nossa confeitaria artesanal. 
+    Nossos <span style="font-style: italic;">bolos</span> são criados com massas leves, 
+    enquanto nossos <span style="font-style: italic;">doces</span> trazem o equilíbrio perfeito. 
+    Oferecemos o cenário ideal para você saborear o que a vida tem de 
+    <strong style="text-transform: uppercase; letter-spacing: 1px;">mais doce</strong>.
+</p>
 
-        <div class="content-column right-column">
-            <div class="inner-column">
-                <h2>Any Candy <br>In Our Stone</h2>
-                <span class="devider_icon_light"></span>
-                <p>Lorem ipsum dolor sit Amet, consectetur adipiscing elit. Suspendisse condimentum nisi et dolor ornare</p>
-                <div class="btn-box">
-                    <a href="#" class="theme-btn btn-style-one large alt"><span></span>Make Order Now<span></span></a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>  
+              </div>
+          </div>
+
+          <!-- Image Column -->
+          <div class="image-column">
+              <div class="image-box">
+                  <figure class="image1"><img src="{{ asset('davilla/IMG/chefe.png') }}" alt=""></figure>
+              </div>
+          </div>
+
+      </div>
+      </div>
+  </section>
+  <!--End Fluid Section one -->
